28702c41f0d9a528b2991ee29c5544b0c7b67c9f
Michael-Hart-2000 <20089696@mail.wit.ie>
2022-12-05 16:22:45 -0800

    Write skeleton menu for Gallery App

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,60 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+import java.lang.System.exit
+import java.util.*
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+val scanner = Scanner(System.`in`)
+
+fun main(args: Array<String>) {
+    runMenu()
+}
+
+fun mainMenu() : Int {
+    print("""
+          > ----------------------------------
+          > |          GALLERY APP           |
+          > ----------------------------------
+          > | ARTIST MENU                    |
+          > |   1) Add an artist             |
+          > |   2) List all artists          |
+          > |   3) Update an artist          |
+          > |   4) Delete an artist          |
+          > ----------------------------------
+          > |   0) Exit                      |
+          > ----------------------------------
+          > ==>> """.trimMargin(">"))
+    return scanner.nextInt()
+}
+
+fun runMenu() {
+    do {
+        val option = mainMenu()
+        when (option) {
+            1  -> addArtist()
+            2  -> listArtists()
+            3  -> updateArtist()
+            4  -> deleteArtist()
+            0  -> exitApp()
+            else -> println("Invalid option entered: ${option}")
+        }
+    } while (true)
+}
+
+fun addArtist(){
+    println("You chose Add Artist")
+}
+
+fun listArtists(){
+    println("You chose List Artists")
+}
+
+fun updateArtist(){
+    println("You chose Update Artist")
+}
+
+fun deleteArtist(){
+    println("You chose Delete Artist")
+}
+
+fun exitApp(){
+    println("Exiting...bye")
+    exit(0)
 }
